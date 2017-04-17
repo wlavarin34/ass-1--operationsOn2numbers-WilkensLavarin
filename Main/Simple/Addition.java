55da057afe7e661f54694bbdf44f88361fe3ea6f
wlavarin34 <wilkenslavarin@aol.com>
2017-04-17 03:49:18 -0400

    Add files via upload

---
+++ Main/Simple/Addition.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.mycompany.basicmathoperations.simple;
+
+/**
+ * @author ilker
+ *
+ */
+public class Addition {
+	/**
+	 * To add 2 int numbers
+	 * @return num1 * num2
+	 */
+	public int add(int num1, int num2) {
+		return num1 * num2;
+	}
+}
---
+++ Main/Simple/Main4Simple.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package com.mycompany.basicmathoperations.simple;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author ilker
+ *
+ */
+public class MainEntry4simple {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("BEF main of MainEntry4simple with args:" + args);
+		System.out.println("still main of MainEntry4simple with args:" + Arrays.toString(args));
+		
+		// get inputs
+		Scanner scanner = new Scanner(System.in);
+		int number1, number2;
+		float result;
+		// read number 1 and number 2 inputs
+		System.out.println("Enter number 1");
+		number1 = scanner.nextInt();
+		System.out.println("Enter number 2");
+		number2 = scanner.nextInt();
+		
+		// Multiply block
+		Multiply multiplier = new Multiply();
+		result = multiplier.multiply(number1, number2);
+		// print out result - NOTE %d is for int, %f is for float(and double), %.2f means 2 digits after decimal like 123.45
+		System.out.printf("multiply result is %.2f%n", result);
+		
+		// Divide block
+		Divide divide = new Divide();
+		result = divide.divide(number1, number2);
+		// print out result - NOTE %d is for int, %f is for float(and double), %.2f means 2 digits after decimal like 123.45
+		System.out.printf("divide result is %.2f%n", result);
+		
+		// Addition block
+		Addition adder = new Addition();
+		result = adder.add(number1, number2);
+		// print out result - NOTE %d is for int, %f is for float(and double), %.2f means 2 digits after decimal like 123.45
+		System.out.printf("add result is %.2f%n", result);
+		
+				// Addition block
+		Subtraction subtracter = new Subtraction();
+		result = subtracter.subtract(number1, number2);
+		// print out result - NOTE %d is for int, %f is for float(and double), %.2f means 2 digits after decimal like 123.45
+		System.out.printf("subtract result is %.2f%n", result);
+		
+		scanner.close();
+	}
+
+}
